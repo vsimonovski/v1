663effd6dd56e810deb554e75802fe32e2df8412
vsimonovski <vladimir.simonovski@kindredgroup.com>
2020-03-01 19:59:49 +0100

    feat: introduced testing

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,13 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+it('should match snapshot', () => {
+  const tree = render(<App />);
+  expect(tree).toMatchSnapshot();
 });
+
+// it('renders learn react link', () => {
+//   const { getByText } = render(<App />);
+//   const linkElement = getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
